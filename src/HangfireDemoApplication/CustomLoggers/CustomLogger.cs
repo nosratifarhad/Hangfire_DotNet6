e490102673d65031f72324d0c6c852df601ddbf4
f_nosrati <f_nosrati@SHL-62>
2023-05-21 21:10:36 +0330

    Add files

---
+++ src/HangfireDemoApplication/CustomLoggers/CustomLogger.cs
@@ -0,0 +1,27 @@
+﻿using Hangfire.Logging;
+
+namespace HangfireDemoApplication.CustomLoggers;
+
+public class CustomLogger : ILog
+{
+    public string Name { get; set; }
+
+    public bool Log(Hangfire.Logging.LogLevel logLevel, Func<string> messageFunc, Exception exception = null)
+    {
+        if (messageFunc == null)
+        {
+            return logLevel > Hangfire.Logging.LogLevel.Info;
+        }
+
+        Console.WriteLine(String.Format("{0}: {1} {2} {3}", logLevel, Name, messageFunc(), exception));
+
+        return true;
+    }
+}
+public class CustomLogProvider : ILogProvider
+{
+    public ILog GetLogger(string name)
+    {
+        return new CustomLogger { Name = name };
+    }
+}
