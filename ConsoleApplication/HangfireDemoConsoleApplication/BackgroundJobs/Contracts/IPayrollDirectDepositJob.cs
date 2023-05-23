d672c47c352ecb42c69d46fe70a5e030132c9a4a
f_nosrati <f_nosrati@SHL-62>
2023-05-23 18:19:42 +0330

    Remove console paroject

--- ConsoleApplication/HangfireDemoConsoleApplication/BackgroundJobs/Contracts/IPayrollDirectDepositJob.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HangfireDemoConsoleApplication.BackgroundJobs.Contracts;
-
-public interface IPayrollDirectDepositJob
-{
-    Task MonthlyPayrollDirectDeposit();
-
-    Task PayrollDirectDeposit(int userId);
-}
